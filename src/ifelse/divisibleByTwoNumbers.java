3b9554b84fec33b830628a68afd7f0c32f6bdf11
Chethan <gchetan8008@gmail.com>
2024-06-12 19:48:14 +0530

    done with third program

---
+++ src/ifelse/divisibleByTwoNumbers.java
@@ -0,0 +1,18 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class divisibleByTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = scan.nextInt();
+        // This program checks wheather the number is divisible by 3 and 5 or not
+        if (n % 3 == 0 && n % 5 == 0) {
+            System.out.println("The number is divisible by 3 and 5");
+        } else {
+            System.out.println("The number is not divisible by 3 and 5");
+        }
+        scan.close();
+    }
+}
