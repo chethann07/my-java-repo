77c79a74c83cea58cc060272deaf1ee864c2d39d
Chethan <gchetan8008@gmail.com>
2024-06-12 19:33:39 +0530

    done with first program

--- src/ifelse/divisibleByTwo.java
+++ src/ifelse/divisibleByTwo.java
@@ -1,5 +1,17 @@
 package ifelse;
 
-public class divisibleByTwo {
+import java.util.Scanner;
 
+public class divisibleByTwo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("Divisible by 2");
+        } else {
+            System.out.println("Not divisible by 2");
+        }
+        scan.close();
+    }
 }
