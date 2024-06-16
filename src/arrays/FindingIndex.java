59fd3aebc96f2136bdc6db298776df764c3cb33b
Chethan <gchetan8008@gmail.com>
2024-06-16 09:38:55 +0530

    sixth program of arrays section

---
+++ src/arrays/FindingIndex.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class FindingIndex {
+
+    public static void findMaxIndex(int[] arr) {
+        int index = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+        System.out.println("The maximum element in the array is : " + max);
+        System.out.println("The index of maximum element in the array is : " + index);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.println("The elements of the array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        findMaxIndex(arr);
+        scan.close();
+    }
+}
