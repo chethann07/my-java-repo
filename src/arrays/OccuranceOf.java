1c14f441b7b371f1fe53f8e6e2a59f7c59c99698
Chethan <gchetan8008@gmail.com>
2024-06-16 09:51:08 +0530

    seventh program of arrays section

--- src/arrays/OccuranceOf.java
+++ src/arrays/OccuranceOf.java
@@ -1,5 +1,34 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class OccuranceOf {
 
+    public static int findingOccurance(int[] arr, int k) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (k == arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.println("Enter the element that you need to search the occurance of : ");
+        int k = scan.nextInt();
+        int res = findingOccurance(arr, k);
+        System.out
+                .println(
+                        "The number of times that the given number " + k + " has appeared inside the array is :" + res);
+        scan.close();
+    }
 }
