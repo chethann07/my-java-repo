a263fe05336c7a630ebdca05976c158cb9442deb
Chethan <gchetan8008@gmail.com>
2024-06-27 22:52:21 +0530

    factors program

--- src/loops/FactorsOfNum.java
+++ src/loops/FactorsOfNum.java
@@ -18,6 +18,15 @@ public class FactorsOfNum {
         System.out.println("The number of factors of n are : " + count);
     }
 
+    /*
+     * this program prints the factors of n and also the number of factors
+     * Input:
+     * Enter the value of n :
+     * 18
+     * Output:
+     * The factors of n are :
+     * 1 2 3 6 9 18
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the value of n :");
